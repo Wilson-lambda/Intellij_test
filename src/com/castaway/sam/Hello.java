c23ae93ed7fd76a8eb7d3fc60200ed835bc51cc0
samkala <monkey.d.samkala@gmail.com>
2016-07-02 15:50:24 +0800

    Initial commit

---
+++ src/com/castaway/sam/Hello.java
@@ -0,0 +1,10 @@
+package com.castaway.sam;
+
+/**
+ * Created by Nicolas on 2016/7/2.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("hello wold");
+    }
+}
